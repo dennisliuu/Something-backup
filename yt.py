061150012a5067b6f0f2af67121b1607661de35b
dennisliuu <dennisliuu@gmail.com>
2017-01-14 22:58:12 +0800

    yt

--- yt.py
+++ yt.py
@@ -2,12 +2,18 @@ import subprocess
 import shlex
 import os
 
-a = input("Name: ")
-b = input("Code: ")
-print ("Target: " + a + ".mp3")
+a = input("This song name: ")
+b = input("The youtube code: ")
+
 
 os.system("youtube-dl -F https://www.youtube.com/watch?v=%s" % str(b))
-fm = input("Format: ")
-os.system("youtube-dl -f %s https://www.youtube.com/watch?v=%s" % (str(fm),str(b)))
-os.system("ffmpeg -i *.webm -acodec libmp3lame -aq 4 %s.mp3" % str(a))
+fm = input("Format code: ")                                                                  os.system("youtube-dl -f %s https://www.youtube.com/watch?v=%s" % (str(fm),str(b)))
+print("webm now is ok!")
+yes = input("Do you want to Format?(y/N)")
 
+if str(yes) == "y" :
+	typ = input("What type? ")
+	os.system("ffmpeg -i *.webm -acodec libmp3lame -aq 4 %s.%s" % (str(a),str(typ)))
+	os.system("rm *.webm")
+else:
+	exit()
