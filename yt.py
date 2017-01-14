b34768ecffdc12bb5f58febc6e86c0a7f6a3e9a8
dennisliuu <dennisliuu@gmail.com>
2017-01-14 22:33:32 +0800

    yt

--- yt.py
+++ yt.py
@@ -1,7 +1,13 @@
 import subprocess
 import shlex
+import os
+
 a = input("Name: ")
-b = input("Code:  ")
+b = input("Code: ")
 print ("Target: " + a + ".mp3")
-subprocess.call(shlex.split('./yt.sh %s %s' % (str(a),str(b))))
+
+os.system("youtube-dl -F https://www.youtube.com/watch?v=%s" % str(b))
+fm = input("Format: ")
+os.system("youtube-dl -f %s https://www.youtube.com/watch?v=%s" % (str(fm),str(b)))
+os.system("ffmpeg -i *.webm -acodec libmp3lame -aq 4 %s.mp3" % str(a))
 
