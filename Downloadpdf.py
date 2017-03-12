1d9b07b6e1d415e5bb5c0de687a080039374fe76
dennisliuu <dennisliuu@gmail.com>
2017-03-12 23:06:43 +0800

    init

--- Downloadpdf.py
+++ Downloadpdf.py
@@ -1,18 +1,23 @@
 import urllib.request, os
+import webbrowser
 
-pathname = input('Creat a floder:')
-start = int(input('Start: '))
-end = int(input('End: '))
+# -*- coding: utf8 -*-
 
-os.system('mkdir %s' % pathname)
+# for i in range(1,11):
+# 	webbrowser.open(urllib.request.unquote('http://web.phys.ntu.edu.tw/ap/102hangout/grad2/Home%20Work%20Solutions%20%s.pdf') % i) 
 
-for i in range(start, end + 1):
-	url = 'http://phys.thu.edu.tw/~mengwen/UniversityPhysics/Halliday-problem/100-homework-Ch%s.pdf' % i
+
+# pathname = input('Creat a floder:')
+# start = int(input('Start: '))
+# end = int(input('End: '))
+# os.system('mkdir %s' % pathname)
+for i in range(1, 10 + 1):	
+	url = 'http://web.phys.ntu.edu.tw/ap/102hangout/grad2/Home%20Work%20Solutions%20%s.pdf' % i
 	# Download the file from `url` and save it locally under `file_name`:
 	try:
-		with urllib.request.urlopen(url) as response, open('./%s/100-homework-Ch%s.pdf' % (pathname, i), 'wb') as out_file:
+		with urllib.request.urlopen(url) as response, open('Home Work Solutions %s.pdf' % i, 'wb') as out_file:
 		    data = response.read() # a `bytes` object
 		    out_file.write(data)
 	except:
 		print('404')
-		i=i+1
+	
