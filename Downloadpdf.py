131d526c883b6583c468b21fe88073a5746cc862
dennisliuu <dennisliuu@gmail.com>
2017-03-12 23:20:26 +0800

    init

--- Downloadpdf.py
+++ Downloadpdf.py
@@ -12,7 +12,7 @@ import webbrowser
 # end = int(input('End: '))
 # os.system('mkdir %s' % pathname)
 for i in range(1, 10 + 1):	
-	url = 'http://web.phys.ntu.edu.tw/ap/102hangout/grad2/Home%20Work%20Solutions%20%s.pdf' % i
+	url = "http://web.phys.ntu.edu.tw/ap/102hangout/grad2/Home%20Work%20Solutions%20" + str(i) + ".pdf"
 	# Download the file from `url` and save it locally under `file_name`:
 	try:
 		with urllib.request.urlopen(url) as response, open('Home Work Solutions %s.pdf' % i, 'wb') as out_file:
